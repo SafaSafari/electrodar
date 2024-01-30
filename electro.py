e22ab4e81c72d64c807581ad8068a3b48d31ec87
SafaSafari <SafaSafari.ss@gmail.com>
2024-01-30 21:28:44 +0330

    Update radar game api

--- electro.py
+++ electro.py
@@ -3,34 +3,57 @@ import json
 import requests
 
 
-def ss_input(prompt, default='', t=int):
-    result = input('{}{}: '.format(
-        prompt, (" [" + str(default) + "] (Enter for default)") if str(default) != '' else ''))
-    if result == '':
+def ss_input(prompt, default="", t=int):
+    result = input(
+        "{}{}: ".format(
+            prompt,
+            (" [" + str(default) + "] (Enter for default)")
+            if str(default) != ""
+            else "",
+        )
+    )
+    if result == "":
         return default
     else:
         return t(result)
 
 
-class Radar:
-
+class Electro:
     def run(self):
         l = self.getList()
         for k, v in enumerate(l):
-            print("{}. {}".format(k + 1, v['name']))
-        server = l[ss_input("Please select server", 1) - 1]
-        self.url = server['configLink']
-        self.api = server['apiLink']
-        print(self.genConfig(True))
+            print("{}. {}".format(k + 1, v["name"]))
+        i = ss_input("Please select server", 1) - 1
+        server = l[i]
+        self.url = server["configLink"]
+        self.api = server["apiLink"]
+        name = str(uuid.uuid4())
+        with open(name, "w") as f:
+            f.write(self.genConfig(True))
+        print("Conf file saved successfully: {}".format(name))
 
     def getList(self):
-        return json.loads(requests.get("https://elcdn.ir/app/servers.json", headers={"User-Agent": ""}).content.replace(b'\n', b'').replace(b',]', b']'))
+        return json.loads(
+            requests.get(
+                "https://elcdn.ir/app/servers.json", headers={"User-Agent": ""}
+            )
+            .content.replace(b"\n", b"")
+            .replace(b",]", b"]")
+        )
 
     def publicInfo(self):
-        return json.loads(requests.get(self.url, headers={"User-Agent": ""}).content.replace(b'\n', b''))
+        return json.loads(
+            requests.get(self.url, headers={"User-Agent": ""}).content.replace(
+                b"\n", b""
+            )
+        )
 
     def privateInfo(self):
-        return json.loads(requests.get(self.api, headers={"User-Agent": ""}).content.replace(b'\n', b''))
+        return json.loads(
+            requests.get(self.api, headers={"User-Agent": ""}).content.replace(
+                b"\n", b""
+            )
+        )
 
     def genConfig(self, route=False):
         base = """[Interface]
@@ -46,12 +69,18 @@ AllowedIPs = {route}
 """
         public = self.publicInfo()
         private = self.privateInfo()
-        base = base.format(public=public['publickey'], endpoint=public['endpoint'], dns=public['dns'],
-                           route="0.0.0.0/0" if route else public['routes'], psk=private['psk'], private=private['private_key'], address=private['ip'])
-        with open(str(uuid.uuid4()) + ".conf", 'w') as f:
-            f.write(base)
-            return "Conf file saved successfully: {}".format(f.name)
+        base = base.format(
+            public=public["publickey"],
+            endpoint=public["endpoint"],
+            dns=public["dns"],
+            route="0.0.0.0/0" if route else public["routes"],
+            psk=private["psk"],
+            private=private["private_key"],
+            address=private["ip"],
+        )
+        return base
 
-if __name__ == '__main__':
-    r = Radar()
+
+if __name__ == "__main__":
+    r = Electro()
     r.run()
--- radar.py
+++ radar.py
@@ -1,35 +1,52 @@
+import uuid
 import requests
 
 
-def ss_input(prompt, default='', t=int):
-    result = input('{}{}: '.format(
-        prompt, (" [" + str(default) + "] (Enter for default)") if str(default) != '' else ''))
-    if result == '':
+def ss_input(prompt, default="", t=int):
+    result = input(
+        "{}{}: ".format(
+            prompt,
+            (" [" + str(default) + "] (Enter for default)")
+            if str(default) != ""
+            else "",
+        )
+    )
+    if result == "":
         return default
     else:
         return t(result)
 
 
 class Radar:
+    def __init__(self, old=False) -> None:
+        self.base = "https://gw.radar.game" if old else "https://prod.radar.game"
+        self.session = requests.Session()
 
     def run(self):
         l = self.getList()
         for k, v in enumerate(l):
-            print("{}. {}".format(k + 1, v['loc']))
-        server = l[ss_input("Please select server", 1) - 1]
-        self.url = server['domain']
-        self.dns = ','.join(server['dns'])
-        print(self.genConfig(True))
+            print("{}. {}, ping: {}".format(k + 1, v["loc"], v["ping"]))
+        i = ss_input("Please select server", 1) - 1
+        server = l[i]
+        self.url = server["domain"]
+        self.dns = ",".join(server["dns"])
+        name = str(uuid.uuid4())
+        with open(name, "w") as f:
+            f.write(self.genConfig(True))
+        print("Conf file saved successfully: {}".format(name))
 
     def getList(self):
-        return requests.get("https://gw.radar.game/list", headers={"User-Agent": ""}).json()['result']
+        return self.session.get(self.base + "/list", headers={"User-Agent": ""}).json()[
+            "result"
+        ]
 
     def publicInfo(self):
-        return requests.get(
-            "https://gw.radar.game/getSettings", headers={"User-Agent": ""}).json()
+        return self.session.get(
+            self.base + "/getSettings", headers={"User-Agent": ""}
+        ).json()
 
     def privateInfo(self):
-        return requests.get(self.url, headers={"User-Agent": ""}).json()
+        return self.session.get(self.url, headers={"User-Agent": ""}).json()
 
     def genConfig(self, route=False):
         base = """[Interface]
@@ -45,12 +62,18 @@ AllowedIPs = {route}
 """
         public = self.publicInfo()
         private = self.privateInfo()
-        base = base.format(public=private['result']['settings']['public_key'], endpoint=private['result']['settings']['endpoint'], dns=self.dns,
-                           route="0.0.0.0/0" if route else public['result']['routes'], psk=private['result']['psk'], private=private['result']['private_key'], address=private['result']['ip'])
-        with open(private['result']['uid'] + ".conf", 'w') as f:
-            f.write(base)
-            return "Conf file saved successfully: {}".format(f.name)
+        base = base.format(
+            public=private["result"]["settings"]["public_key"],
+            endpoint=private["result"]["settings"]["endpoint"],
+            dns=self.dns,
+            route="0.0.0.0/0" if route else public["result"]["routes"],
+            psk=private["result"]["psk"],
+            private=private["result"]["private_key"],
+            address=private["result"]["ip"],
+        )
+        return base
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     r = Radar()
     r.run()
